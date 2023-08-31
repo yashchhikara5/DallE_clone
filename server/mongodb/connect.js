dee0a7f865aefa989a00fd53c832cc5d06f7accd
yashchhikara5 <yashchhikara5@gmail.com>
2023-08-31 15:19:24 +0530

    final changes

--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -5,7 +5,10 @@ const connectDB = (url) => {
 
     mongoose.connect(url)
         .then(() => console.log('MongoDB connected'))
-        .catch((err) => console.log(err));
+        .catch((err) => {
+            console.error('failed to connect with mongo');
+            console.error(err);
+        });
 }
 
 export default connectDB;
